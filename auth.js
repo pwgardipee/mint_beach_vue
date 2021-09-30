d2fb239f7fc9c14a7b1133c318027ddd86cb3b14
Peyton Gardipee <peytongardipee@gmail.com>
2021-09-30 09:11:56 -0700

    Add auth.signup method.

--- auth.js
+++ auth.js
@@ -1,27 +1,37 @@
 import { mb } from "mint_beach_js";
 import { reactive, toRefs } from "vue";
 
+const STORAGE_TOKEN = "mb_tok";
+
 const state = reactive({
   currentUser: undefined,
 });
 
 export default function initAuth(config) {
   const MB = mb(config.tenantId, config.tenantId);
+
   const login = async (email, password) => {
     const l = await MB.auth.login(email, password);
-    localStorage.setItem("mb_tok", l.data.tokenManager.accessToken);
+    localStorage.setItem(STORAGE_TOKEN, l.data.tokenManager.accessToken);
     await checkAuth();
     return l;
   };
 
   const logout = async () => {
-    localStorage.removeItem("mb_tok");
+    localStorage.removeItem(STORAGE_TOKEN);
     await checkAuth();
+  };
+
+  const signup = async (email, password, name) => {
+    const s = await MB.auth.signup(email, password, name);
+    localStorage.setItem(STORAGE_TOKEN, s.data.tokenManager.accessToken);
+    await checkAuth();
+    return s;
   };
 
   const checkAuth = async () => {
     try {
-      const token = localStorage.getItem("mb_tok");
+      const token = localStorage.getItem(STORAGE_TOKEN);
       if (!token) throw new Error();
       MB.auth.setAuthHeading(token);
       const me = await MB.auth.me();
@@ -35,6 +45,7 @@ export default function initAuth(config) {
     ...toRefs(state), // convert to refs when returning
     login,
     logout,
+    signup,
     checkAuth,
   };
 }
--- index.js
+++ index.js
@@ -7,12 +7,13 @@ export default {
     //Initialize base URL for API
     init(options.baseURL);
 
-    const { currentUser, checkAuth, login, logout } = initAuth(options);
+    const { currentUser, checkAuth, login, logout, signup } = initAuth(options);
 
     //Add $mb and $mb_admin to Vue's gobal properties
     Vue.config.globalProperties.$mb = mb(options.tenantId, options.tenantId);
     Vue.config.globalProperties.$mb.auth.login = login;
     Vue.config.globalProperties.$mb.auth.logout = logout;
+    Vue.config.globalProperties.$mb.auth.signup = signup;
     Vue.config.globalProperties.$mb.auth.getCurrentUser = () => currentUser;
     Vue.config.globalProperties.$mb_admin = mb_admin;
 
